772daa60f99cb563b7353f07fd934b36c1986bed
deanamiel <deanamiel1@gmail.com>
2022-02-14 01:06:38 -0500

    Uploading to GitHub

---
+++ ethereum/deploy.js
@@ -0,0 +1,23 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'save peanut network immense derive hobby genius cross envelope action chef outer',
+  'https://rinkeby.infura.io/v3/e9412a5afd984755a44934e10883b9be'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
